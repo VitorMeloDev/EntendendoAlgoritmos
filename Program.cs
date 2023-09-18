7a9131a57c3a77707cad8a95d28a33e6f86dbccd
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2023-09-18 06:37:37 -0300

    Primeiro commit: Pesquisa Binaria

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using EntendendoAlgoritmos.Services;
+using System;
+
+namespace EntendendoAlgoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 7, 10, 23, 45, 35, 66 };
+
+            Console.WriteLine("\nO valor está na posição: " + PesquisaBinaria.Pesquisa(ints, 23));
+        }
+    }
+}
---
+++ Services/PesquisaBinaria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntendendoAlgoritmos.Services
+{
+    class PesquisaBinaria
+    {
+        public static int Pesquisa(int[] list, int item) // Aqui darei a lista e o item a ser buscado
+        {
+            // Baixo e Alto acompanham os dois extremos da lista, o menor e maior valor
+            int baixo = 0;
+            int alto = list.Length - 1;
+            int ciclo = 1;
+
+            while (baixo <= alto)
+            {
+
+                Console.WriteLine("Ciclo " + ciclo);
+
+                int meio = (baixo + alto) / 2;
+                Console.WriteLine("Meio de "+ baixo + " e " + alto + ": " + meio);
+
+                double chute = list[meio];
+                Console.WriteLine("O VALOR PARA O CHUTE É: " + chute);
+
+                if (chute == item)
+                {
+                    return meio;
+                }
+
+                if(chute > item)
+                {
+                    Console.Write("Chute foi alto, incrementando a variavel ALTO de: " + alto + ", para: ");
+                    alto = meio - 1;
+                    Console.WriteLine(alto);
+                }
+                else
+                {
+                    Console.Write("Chute foi baixo, incrementando a variavel BAIXO de: " + baixo + ", para: ");
+                    baixo = meio + 1;
+                    Console.WriteLine(baixo);
+                }
+
+                ciclo++;
+                Console.WriteLine();
+            }
+            return -1;
+        }
+    }
+}
